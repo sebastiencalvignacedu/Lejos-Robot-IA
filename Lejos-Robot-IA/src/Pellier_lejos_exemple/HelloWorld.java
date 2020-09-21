59e3c79d23a13f52a0bde80ff2a34167596d387d
Sebastien Calvignac <sebastien.calvignac@etu.univ-grenoble-alpes.fr>
2020-09-21 18:19:51 +0200

    Code exemple de Damien Pellier

---
+++ Lejos-Robot-IA/src/Pellier_lejos_exemple/HelloWorld.java
@@ -0,0 +1,23 @@
+package Pellier_lejos_exemple;
+
+
+import lejos.hardware.Button;
+import lejos.utility.Delay;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try {
+			
+		System.out.println("Hello World!");
+		Button.ENTER.waitForPress();
+		
+		} catch (Throwable t) {
+			t.printStackTrace();
+			Delay.msDelay(10000);
+			System.exit(0);
+		}
+	}
+
+}
---
+++ Lejos-Robot-IA/src/Pellier_lejos_exemple/LineFollower.java
@@ -0,0 +1,17 @@
+package Pellier_lejos_exemple;
+
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
+
+
+public class LineFollower
+{
+    public static void main(String[] args)
+    {
+        Controller controller = new Controller(SensorPort.S4, MotorPort.B, MotorPort.C);
+        
+        controller.run();
+        
+    }
+}
