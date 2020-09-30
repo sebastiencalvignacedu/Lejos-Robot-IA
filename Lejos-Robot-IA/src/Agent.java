3e0d6253eadf2fb08e80a171d38887dcc4b5bc49
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-09-30 23:10:13 +0200

    Centralisation des noms de ports "S2", "S3" etc.
    "Dictionnaire" des ports pour pouvoir centraliser le changement des ports dans le code. Dans les autres classes, utiliser donc Agent.ultraPort à la place de "S2" par exemple.

--- Lejos-Robot-IA/src/Agent.java
+++ Lejos-Robot-IA/src/Agent.java
@@ -1,4 +1,8 @@
 //premier commit
 public class Agent {
-
+  
+  // Centralisation des noms de ports
+	protected static String ultraPort = "S2";
+	protected static String colorPort = "S3";
+	protected static String touchPort = "S4";
 }
