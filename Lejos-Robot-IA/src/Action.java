a36b139d9b28bad53845332f3c90fc1f399b547a
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-10-04 13:15:16 +0200

    Attributs et signature des methodes de la classe Action

--- Lejos-Robot-IA/src/Action.java
+++ Lejos-Robot-IA/src/Action.java
@@ -1,5 +1,48 @@
-// re first
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.Port;
+
 public class Action {
-//kamissoko 2
-	// my modification seb
+	Port A;
+	
+	private EV3LargeRegulatedMotor LeftMotor, RightMotor;
+	
+	private EV3MediumRegulatedMotor MiddleMotor;
+	
+	private float orientation; // stocker historique des rotations pour savoir ou vise le robot. 
+	
+	public void avancer() {
+		
+	}
+	
+	public void reculer() {
+		
+	}
+	
+	public void tournerHoraire() {
+		
+	}
+	
+	public void tournerAntiHoraire() {
+		
+	}
+	
+	public void pincer(){
+		
+	}
+	
+	public void desserer() {
+		
+	}
+	
+	public void clignoter() {
+		
+	}
+	
+	public void klaxon() {
+		
+	}
+
 }
