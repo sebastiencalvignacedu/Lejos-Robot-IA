46392df271f6a77e3ae78d881c675a0a9ba9b315
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-10-04 13:03:33 +0200

    retour sur le dernier commit "centralisation ..."

--- Lejos-Robot-IA/src/Agent.java
+++ Lejos-Robot-IA/src/Agent.java
@@ -1,8 +1,4 @@
 //premier commit
 public class Agent {
   
-  // Centralisation des noms de ports
-	protected static String ultraPort = "S2";
-	protected static String colorPort = "S3";
-	protected static String touchPort = "S4";
 }
