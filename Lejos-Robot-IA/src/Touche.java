7cf580b9c570bacabd90930cd0c4d0f9acb0ceef
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-11-26 09:03:46 +0100

    Update Touche.java

--- Lejos-Robot-IA/src/Touche.java
+++ Lejos-Robot-IA/src/Touche.java
@@ -1,48 +1,17 @@
-
+import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.robotics.SampleProvider;
-/**
- * <b> La classe Touche represente ......</b>
- * <p>
- * La classe Touche hérite de la Capteurs
- * description de la classe ????????
- * </p>
- * @author membre du Groupe
- * @version 8.0
- * 
- */
-public class Touche extends Capteurs {
-	/**
-	 * 	@see Port#s4
-	 * Le port s4 est le numéro de port de la touche.
-	 */
+
+public class Touche /*extends Capteurs*/ {
+	
 	Port s4;
-	
-	/**
-	 * @see EV3TouchSensor
-	 */
 	EV3TouchSensor buteeCapteur;
-	
-	/**
-	 * @see SampleProvider
-	 */
 	SampleProvider recup;
-	
-	/**
-	 * Le "echantillon" de la touche. Est un tableau qui contient la valeur des echantillon.
-	 */
 	float[] echantillon;
-	
-	/**
-	 * La "touche" de la Touche. Detecte la presence d'un objet.
-	 */
 	boolean touche;
-	
-	/**
-	 * Constructeur Touche Vide.
-	 */
+
 	public Touche() {
 		s4 = LocalEV3.get().getPort("S4");
 		buteeCapteur = new EV3TouchSensor(s4);
@@ -53,13 +22,19 @@ public class Touche extends Capteurs {
 		//Delay.msDelay(250);
 		//touchSensor.close();
 	}
-	/**
-	 * actualise de la Touche. Permet d'actualiser la "touche".
-	 */
+	
 	public void actualise() {
 		recup.fetchSample(echantillon,0);
 		//touchSensor.close();
-		touche = ((int)echantillon[0]==1);	
-	}
+		touche = echantillon[0]==1;	
+		}
+	
+	/*public static void main(String[] args) {
+		Touche t = new Touche();
+		while (Button.ESCAPE.isUp())  {
+			t.actualise();
+			System.out.println(t.touche);
+		}
+	}*/
 
 }
