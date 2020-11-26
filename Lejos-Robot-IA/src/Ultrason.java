881a922c1edfbab00756b3b40bc075d3f39d3a8e
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-11-26 09:04:14 +0100

    Update Ultrason.java

--- Lejos-Robot-IA/src/Ultrason.java
+++ Lejos-Robot-IA/src/Ultrason.java
@@ -1,50 +1,19 @@
-
+import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
-/**
- * <b> La classe Ultrason represente ......</b>
- * <p>
- * description de la classe ????????
- * </p>
- * @author membre du Groupe
- * @version 8.0 
- */
-public class Ultrason extends Capteurs {
-	/**
-	 * @see Port
-	 * Le s2 est le numero de port de Ultrason.
-	 */
+
+public class Ultrason /*extends Capteurs*/ {
+
 	Port s2;
-	
-	/**
-	 * @see EV3UltrasonicSensor
-	 * Le "ultraCapteur" est le capteur de ultraSon
-	 */
 	EV3UltrasonicSensor ultraCapteur;
-	
-	/**
-	 * @see SampleProvider
-	 * 
-	 */
 	SampleProvider recup;
-	
-	/**
-	 * Le "echantillon" est un tableau qui contient la valeur des echantillons.
-	 */
 	float[] echantillon;
-	
-	/**
-	 * La "distance" est la valeur des distances que le robot doit parcourir. 
-	 */
 	float distance;
 	
-	/**
-	 * Constructeur Vide de Ultason
-	 */
 	public Ultrason() {
-		s2 = LocalEV3.get().getPort("S2");
+		s2 = LocalEV3.get().getPort("S1");
 		ultraCapteur = new EV3UltrasonicSensor(s2);
 		recup = ultraCapteur.getDistanceMode();
 		echantillon = new float[recup.sampleSize()];
@@ -52,13 +21,19 @@ public class Ultrason extends Capteurs {
 		actualise();
 		//ultraCapteur.close();
 	}
-	/**
-	 * actualise de UltraSon.
-	 */
+	
 	public void actualise() {
 		recup.fetchSample(echantillon, 0);
 		this.distance = echantillon[0]; 
 		//ultraCapteur.close();
 	}
+	
+	/*public static void main(String[] args) {
+		Ultrason u = new Ultrason();
+		while (Button.ESCAPE.isUp())  {
+			u.actualise();
+			System.out.println(u.distance);
+		}
+	}*/
 
 }
