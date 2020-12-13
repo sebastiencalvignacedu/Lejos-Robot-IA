68b5bf26eb25ee65cf4b14801dd03763e029139e
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2020-12-14 00:11:13 +0100

    Update Ultrason.java
    avec javadoc

--- Lejos-Robot-IA/src/Robot/Ultrason.java
+++ Lejos-Robot-IA/src/Robot/Ultrason.java
@@ -4,15 +4,40 @@ import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
-
+/**
+ * Classe qui permet d'avoir accès au capteur de distance de type Sonar
+ *
+ */
 public class Ultrason /*extends Capteurs*/ {
-
+	/**
+	 * attribut de classe qui définit le port sur lequel est branché le capteur Touche
+	 * @see Port
+	 */
 	Port s2;
+	/**
+	 * Attribut de classe permettant d'interagir avec le capteur
+	 * @see EV3TouchSensor
+	 */
 	EV3UltrasonicSensor ultraCapteur;
+	/**
+	 * Attribut de classe permettant d'obtenir un échantillon du capteur
+	 * @see SampleProvider
+	 */
 	SampleProvider recup;
+	/**
+	 * tableau de float permettant de garder en mémoire la valeur enregistré
+	 */
 	float[] echantillon;
+	/**
+	 * Attribut permettant de stocké sla distance mesurée
+	 */
 	float distance;
-	
+	/**
+	 * définit les attributs s2, ultraCapteur, recup.
+	 * Le tableau echantillon est initialisé avec la taille approprié définit par buteeCapteur
+	 * l'attribut distance lui est initiialisé avec la fonction actualise()
+	 * @constructor
+	 */
 	public Ultrason() {
 		s2 = LocalEV3.get().getPort("S1");
 		ultraCapteur = new EV3UltrasonicSensor(s2);
@@ -22,7 +47,11 @@ public class Ultrason /*extends Capteurs*/ {
 		actualise();
 		//ultraCapteur.close();
 	}
-	
+	/**
+	 * Méthode qui permet de mesurer la distance et de la stocker dans l'attribut distance
+	 * @return void
+	 * @see SampleProvider#fetchSample(float[] sample, int offset)
+	 */
 	public void actualise() {
 		recup.fetchSample(echantillon, 0);
 		this.distance = echantillon[0]; 
