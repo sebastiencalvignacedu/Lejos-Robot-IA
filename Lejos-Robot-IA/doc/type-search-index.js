c66857e4e74c239a2c256fcbe93295c4f4e38851
sebastiencalvignacedu <68340056+sebastiencalvignacedu@users.noreply.github.com>
2020-12-14 22:15:45 +0100

    final javadoc

--- Lejos-Robot-IA/doc/type-search-index.js
+++ Lejos-Robot-IA/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"Robot","l":"Agent_Final"},{"p":"Robot","l":"AgentV2"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"Robot","l":"Capteurs"},{"p":"Robot","l":"Couleur"},{"p":"Robot","l":"Pilote"},{"p":"Robot","l":"Touche"},{"p":"Robot","l":"Ultrason"}]
+typeSearchIndex = [{"p":"Robot","l":"Agent_Final"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"Robot","l":"Capteurs"},{"p":"Robot","l":"Couleur"},{"p":"Robot","l":"Pilote"},{"p":"Robot","l":"Touche"},{"p":"Robot","l":"Ultrason"}]
